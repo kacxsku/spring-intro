20e721f6d1d40f7b5ad37b81d9582fb0e3d39e6f
kacxsku <kacperskurski23@gmail.com>
2022-01-12 17:31:31 +0100

    v1 rep

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -7,7 +7,7 @@ public class MyApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
+        MessageService messageService = applicationContext.getBean("messageService1", MessageService.class);
         MessageService messageService1 = applicationContext.getBean("messageService", MessageService.class);
 
         System.out.println(messageService.getMessage().hashCode());
